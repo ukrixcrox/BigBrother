bc85f9af19492645e3a9676dd27de4e4ba0641c9
falkwitte <krixcrox@duck.com>
2024-02-06 20:54:41 +0100

    broken

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use std::io::Read;
 use pnet::datalink::{Channel, linux};
 use pnet::datalink::ChannelType;
+use pnet::packet::ethernet::EthernetPacket;
+use pnet::packet::Packet;
+
 fn main(){
     let interface = linux::interfaces().into_iter().find(|e| e.name == "enp42s0").unwrap();
 
@@ -13,6 +17,7 @@ fn main(){
         promiscuous: true,
     };
 
+    // create a new channel dealing with layer 2 packets
     let (_, mut rx) = match linux::channel(&interface, config){
         Ok(Channel::Ethernet(tx, rx)) => (tx,rx),
         Ok(_) => panic!("Unhandeld channel type"),
@@ -21,7 +26,22 @@ fn main(){
 
     loop{
         match rx.next(){
-            Ok(packet) => println!("{:?}", packet),
+            Ok(packet) => {
+                let packet = EthernetPacket::new(packet).unwrap();
+                let mut packet_packet = packet.packet();
+                let mut some_string = String::new();
+
+                packet_packet.read_to_string(&mut some_string).unwrap();
+                println!("{}", some_string);
+
+                /*
+                for byte in packet_packet{
+                    print!("{:02X}", byte);
+                }
+                */
+                //println!("{}", std::str::from_utf8(packet_packet).unwrap())
+
+            }
             Err(e) => panic!("An error occurred while reading: {}", e),
         }
     }
