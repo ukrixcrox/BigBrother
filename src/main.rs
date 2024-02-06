976de5ae556efc152e6c4c209ff014202313bb3a
falkwitte <krixcrox@duck.com>
2024-02-06 20:06:40 +0100

    finally collection packages

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,38 @@
-use pcap::Device;
-
-fn main() {
-    let mut cap = Device::lookup().unwrap().unwrap().open().unwrap();
-
-    while let Ok(packet) = cap.next_packet() {
-        println!("received packet! {:?}", packet);
+use pnet::datalink::{Channel, linux};
+use pnet::datalink::ChannelType;
+fn main(){
+    //let interfaces = linux::interfaces();
+    // list all network interfaces
+    /*
+    for interface in interfaces{
+        println!("{}\n{}", interface.name, interface.description);
+        interface.ips.iter().for_each(|e| println!("{:?}", e));
     }
+    */
+
+    let interface = linux::interfaces().into_iter().find(|e| e.name == "enp42s0").unwrap();
+
+    let config = linux::Config{
+        write_buffer_size: 4096,
+        read_buffer_size: 4096,
+        read_timeout: None,
+        write_timeout: None,
+        channel_type: ChannelType::Layer2,
+        fanout: None,
+        promiscuous: true,
+    };
+
+    let (_, mut rx) = match linux::channel(&interface, config){
+        Ok(Channel::Ethernet(tx, rx)) => (tx,rx),
+        Ok(_) => panic!("Unhandeld channel type"),
+        Err(e) => panic!("An error occurred when creating the datalink channel: {}", e),
+    };
+
+    loop{
+        match rx.next(){
+            Ok(packet) => println!("{:?}", packet),
+            Err(e) => panic!("An error occurred while reading: {}", e),
+        }
+    }
+
 }
