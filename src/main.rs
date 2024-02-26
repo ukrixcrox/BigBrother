8b3672ee1e172673097eb9d39bbd097975e8ee6e
Falk-Witte <krixcrox@duck.com>
2024-02-26 14:16:17 +0100

    shamelessly stole code to capture tcp packages

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,102 @@
-use pnet::datalink::ChannelType;
+use pnet::datalink::{ChannelType, NetworkInterface};
 use pnet::datalink::{linux, Channel};
-use pnet::packet::ethernet::EthernetPacket;
+use pnet::packet::ethernet::{EtherTypes, EthernetPacket, MutableEthernetPacket};
+use pnet::packet::ip::{IpNextHeaderProtocol, IpNextHeaderProtocols};
+use pnet::packet::ipv4::Ipv4Packet;
+use pnet::packet::ipv6::Ipv6Packet;
+use pnet::packet::tcp::TcpPacket;
 use pnet::packet::Packet;
-use std::io::Read;
+use pnet::util::MacAddr;
+use std::net::IpAddr;
+
+fn handle_tcp_packet(interface_name: &str, source: IpAddr, destination: IpAddr, packet: &[u8]) {
+    let tcp = TcpPacket::new(packet);
+    if let Some(tcp) = tcp {
+        println!(
+            "[{}]: TCP Packet: {}:{} > {}:{}; length: {}",
+            interface_name,
+            source,
+            tcp.get_source(),
+            destination,
+            tcp.get_destination(),
+            packet.len()
+        );
+    } else {
+        println!("[{}]: Malformed TCP Packet", interface_name);
+    }
+}
+
+fn handle_transport_protocol(
+    interface_name: &str,
+    source: IpAddr,
+    destination: IpAddr,
+    protocol: IpNextHeaderProtocol,
+    packet: &[u8],
+) {
+    match protocol {
+        IpNextHeaderProtocols::Tcp => {
+            handle_tcp_packet(interface_name, source, destination, packet)
+        }
+        _ => /*println!(
+            "[{}]: Unknown {} packet: {} > {}; protocol: {:?} length: {}",
+            interface_name,
+            match source {
+                IpAddr::V4(..) => "IPv4",
+                _ => "IPv6",
+            },
+            source,
+            destination,
+            protocol,
+            packet.len()
+        )*/(),
+    }
+}
+
+fn handle_ipv4_packet(interface_name: &str, ethernet: &EthernetPacket) {
+    let header = Ipv4Packet::new(ethernet.payload());
+    if let Some(header) = header {
+        handle_transport_protocol(
+            interface_name,
+            IpAddr::V4(header.get_source()),
+            IpAddr::V4(header.get_destination()),
+            header.get_next_level_protocol(),
+            header.payload(),
+        );
+    } else {
+        println!("[{}]: Malformed IPv4 Packet", interface_name);
+    }
+}
+
+fn handle_ipv6_packet(interface_name: &str, ethernet: &EthernetPacket) {
+    let header = Ipv6Packet::new(ethernet.payload());
+    if let Some(header) = header {
+        handle_transport_protocol(
+            interface_name,
+            IpAddr::V6(header.get_source()),
+            IpAddr::V6(header.get_destination()),
+            header.get_next_header(),
+            header.payload(),
+        );
+    } else {
+        println!("[{}]: Malformed IPv6 Packet", interface_name);
+    }
+}
+
+fn handle_ethernet_frame(interface: &NetworkInterface, ethernet: &EthernetPacket) {
+    let interface_name = &interface.name[..];
+    match ethernet.get_ethertype() {
+        EtherTypes::Ipv4 => handle_ipv4_packet(interface_name, ethernet),
+        EtherTypes::Ipv6 => handle_ipv6_packet(interface_name, ethernet),
+        _ => /*println!(
+            "[{}]: Unknown packet: {} > {}; ethertype: {:?} length: {}",
+            interface_name,
+            ethernet.get_source(),
+            ethernet.get_destination(),
+            ethernet.get_ethertype(),
+            ethernet.packet().len()
+        )*/(),
+    }
+}
 
 fn main() {
     // get a network interface that is running and is not the loopback
@@ -18,7 +112,7 @@ fn main() {
         write_timeout: None,
         channel_type: ChannelType::Layer2,
         fanout: None,
-        promiscuous: true,
+        promiscuous: false,
     };
 
     // create a new channel dealing with layer 2 packets
@@ -34,23 +128,47 @@ fn main() {
     println!("Start Listening on: {:?}", interface.name);
 
     loop {
+        let mut buf: [u8; 1600] = [0u8; 1600];
+        let mut fake_ethernet_frame = MutableEthernetPacket::new(&mut buf[..]).unwrap();
         match rx.next() {
             Ok(packet) => {
-                let packet = EthernetPacket::new(packet).unwrap();
-                let mut packet_packet = packet.packet();
-                let mut some_string = String::new();
-
-                /*
-                packet_packet.read_to_string(&mut some_string).unwrap();
-                println!("{}", some_string);
-                */
-
-                for byte in packet_packet {
-                    print!("{:02X}", byte);
+                let payload_offset;
+                if interface.is_up()
+                && !interface.is_broadcast()
+                && ((!interface.is_loopback() && interface.is_point_to_point())
+                    || interface.is_loopback())
+            {
+                if interface.is_loopback() {
+                    // The pnet code for BPF loopback adds a zero'd out Ethernet header
+                    payload_offset = 14;
+                } else {
+                    // Maybe is TUN interface
+                    payload_offset = 0;
                 }
-                //println!("{}", std::str::from_utf8(packet_packet).unwrap())
+                if packet.len() > payload_offset {
+                    let version = Ipv4Packet::new(&packet[payload_offset..])
+                        .unwrap()
+                        .get_version();
+                    if version == 4 {
+                        fake_ethernet_frame.set_destination(MacAddr(0, 0, 0, 0, 0, 0));
+                        fake_ethernet_frame.set_source(MacAddr(0, 0, 0, 0, 0, 0));
+                        fake_ethernet_frame.set_ethertype(EtherTypes::Ipv4);
+                        fake_ethernet_frame.set_payload(&packet[payload_offset..]);
+                        handle_ethernet_frame(&interface, &fake_ethernet_frame.to_immutable());
+                        continue;
+                    } else if version == 6 {
+                        fake_ethernet_frame.set_destination(MacAddr(0, 0, 0, 0, 0, 0));
+                        fake_ethernet_frame.set_source(MacAddr(0, 0, 0, 0, 0, 0));
+                        fake_ethernet_frame.set_ethertype(EtherTypes::Ipv6);
+                        fake_ethernet_frame.set_payload(&packet[payload_offset..]);
+                        handle_ethernet_frame(&interface, &fake_ethernet_frame.to_immutable());
+                        continue;
+                    }
+                }
             }
-            Err(e) => panic!("An error occurred while reading: {}", e),
+            handle_ethernet_frame(&interface, &EthernetPacket::new(packet).unwrap());
         }
+        Err(e) => panic!("packetdump: unable to receive packet: {}", e),
     }
+  }
 }
